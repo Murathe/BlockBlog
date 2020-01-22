6ad3caef92bb052e4964ac12dba004f84a3f865c
murathe <isaacsm07p@gmail.com>
2020-01-22 16:38:20 +0300

    Define notification email content

--- app/email.py
+++ app/email.py
@@ -4,11 +4,14 @@ from .import mail
 
 def welcome_message(subject, template, to, **kwargs):
     sender_email = 'mutuma.brian@yahoo.com'
-    
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body = render_template(template + '.txt', **kwargs)
-    email.body = render_template(template + '.html', **kwargs)
+    email.html = render_template(template + '.html', **kwargs)
     mmail.send(email)
 
 def notification_message(subject, template, to, **kwargs):
-    
+    sender_email = 'mutuma.brian@yahoo.com'
+    email = Message(subject, template, to, **kwargs)
+    email.body = render_template(template + '.txt', **kwargs)
+    email.html = render_template(template + '.html', **kwargs)
+    mail.send(email)
