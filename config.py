8041e29c388d55381a5cc22edb7a731e9b73d6bc
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-22 12:08:44 +0300

    added config file

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+
+class Config:
+    debug = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mutumas:Mutuma1234@localhost/'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
