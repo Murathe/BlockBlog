abaddfd5a589d6b9bb3929873681f626a598aa9d
murathe <isaacsm07p@gmail.com>
2020-01-22 11:30:34 +0300

    Create parent config class

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    debug = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cate:era12@localhost/era'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
